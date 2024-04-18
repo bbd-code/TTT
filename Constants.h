b77f5474bda9b4e0261008fe342f546b00b0ab5c
ilya-bondarcuk0-2kx <144292229+ilya-bondarcuk0-2kx@users.noreply.github.com>
2024-04-18 23:50:28 +0300

    Update Constants.h

--- Constants.h
+++ Constants.h
@@ -8,12 +8,20 @@
 
 namespace Socks
 {
-	namespace _const 
-	{
-		static const int max_packet_size = 8192;
-		static const int INVALID_SOCKET = ~0;
-		using int = BOOL;
-	}
+	
+	static const int max_packet_size = 8192;
+	#ifndef _WIN32
+	static const int INVALID_SOCKET = ~0;
+	using int = BOOL;
+	static const int SOCKET_ERROR = -1;
+
+	#ifndef WSAGetLastError()
+		#define WSAGetLastError() errno
+	#endif // ! WSAGetLastError()
+	#ifndef closesocket
+		#define closesocket(fd) close(fd)
+	#endif // !closesocket
+	#endif //! _WIN32
 }
 
 
