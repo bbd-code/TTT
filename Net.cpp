33a05d9895ceb4b9db0922315fe8d42ddcb1624f
ilya-bondarcuk0-2kx <144292229+ilya-bondarcuk0-2kx@users.noreply.github.com>
2024-04-18 23:36:31 +0300

    Update Net.cpp

--- Net.cpp
+++ Net.cpp
@@ -5,16 +5,20 @@ namespace Socks
 
     bool Net::initialize()
     {
+        #ifdef _WIN32
         WSAData ws_data;
         if (WSAStartup(MAKEWORD(2, 2), &ws_data) != 0)
             return false;
         if (LOBYTE(ws_data.wVersion) != 2 || HIBYTE(ws_data.wVersion) != 2)
             return false;
+        #endif // WIN32
         return true;
     }
 
     void Net::shutdown()
     {
+        #ifdef _WIN32
         WSACleanup();
+        #endif //_WIN32
     }
 }
